616df084691b52c663d7622bacc56aa8797e1093
jacguerrag <jacguerrag@gmail.com>
2024-05-05 16:11:21 -0600

    creacion proyecto

---
+++ src/main/java/canvia/curso/automatizacion/pruebas/java_maven_cucumber/App.java
@@ -0,0 +1,13 @@
+package canvia.curso.automatizacion.pruebas.java_maven_cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
